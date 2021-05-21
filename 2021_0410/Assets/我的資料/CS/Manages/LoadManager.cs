a972548477e32c03043d84a2e4768f9f13c08a25
fluffyovo <71476490+fluffyovo@users.noreply.github.com>
2021-05-21 12:26:08 +0800

    0519

--- 2021_0410/Assets/我的資料/CS/Manages/LoadManager.cs
+++ 2021_0410/Assets/我的資料/CS/Manages/LoadManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadManager : MonoBehaviour
@@ -13,7 +10,6 @@ public class LoadManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (_Player == null)
